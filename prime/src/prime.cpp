2cd7ffa4693b49cd220f34a53d4457375ded579d
Alessandro Fumagalli <alexfum@tin.it>
2020-01-30 14:46:26 +0100

    Import in CLion

--- prime/src/prime.cpp
+++ prime/src/prime.cpp
@@ -1,26 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-		int a = 2;       //start from 2
-		long long int b = 1000;     //ends at 1000
+int main()  {
+	int a = 2;
+	long long int b = 1000;
 
-		for (int i = a; i <= b; i++)
-		{
-
-				for (int j = 2; j <= i; j++)
-				{
-						if (!(i%j)&&(i!=j))    //Condition for not prime
-						{
-								break;
-						}
-
-						if (j==i)             //condition for Prime Numbers
-						{
-								cout << i << endl;
-
-						}
-				}
-		}
+	for (int i = a; i <= b; i++) {
+		for (int j = 2; j <= i; j++) {
+			if (!(i%j)&&(i!=j))  {
+				break;
+			}
+			if (j==i) {
+				cout << i << endl;
+			}
+   		}
+	}
 }
