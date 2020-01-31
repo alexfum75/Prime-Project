559ee2940d71f1ec8f4e5f7664626e86433b272c
Alessandro Fumagalli <alexfum@tin.it>
2020-01-31 12:05:47 +0100

    Review from 0

--- src/prime.cpp
+++ src/prime.cpp
@@ -2,12 +2,15 @@
 #include <sys/utsname.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <set>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
     int a = 2;
-    long long int b = 1000;
+    long long int b = 20;
 
     for (int i = a; i <= b; i++) {
         for (int j = 2; j <= i; j++) {
@@ -19,9 +22,21 @@ int main() {
             }
         }
     }
+    set<int> num;
+    num.insert(10);
+    num.insert(20);
+    cout << "Test C++11" << endl;
+    for (set<int>::iterator it = num.begin(); it != num.end(); ++it) {
+        cout << (*it) << endl;
+    }
+
     struct utsname name;
     if (uname(&name))
         exit(1);
-    std::cout << "Ciao da " <<" " << name.sysname <<
-                 " " << name.release << std::endl;
+    std::cout << "Ciao da " << " " << name.sysname <<
+              " " << name.release << std::endl;
+
+    ofstream outputFile("/data/PolluxFts/newFile.txt");
+    outputFile << "writing to file";
+    outputFile.close();
 }
